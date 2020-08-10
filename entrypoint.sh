28ee2c4022ff1490e699ed401f77865ee6604dfa
Matteo Costantino <matteo@loudsrl.com>
2020-08-10 17:03:13 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -x
 
-echo n | $ANDROID_HOME/tools/bin/avdmanager create avd \
-    -k "system-images;android-${API_LEVEL};" \
-    -n testing -b "${IMG_TYPE};${SYS_IMG}"
-(
-  # Enable keyboard support in QEMU (for VNC)
-  echo 'hw.keyboard = true';
-) >> /root/.android/avd/testing.avd/config.ini
+$ANDROID_HOME/tools/bin/avdmanager create avd \
+    -k "system-images;android-${API_LEVEL};${IMG_TYPE};${SYS_IMG}" \
+    -n testing
 
 exec $*
