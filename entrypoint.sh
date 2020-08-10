642077441946294e54ba4e543d81fd7e2c5da61f
Matteo Costantino <matteo@loudsrl.com>
2020-08-10 17:07:14 +0200

    git

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -x
 
-$ANDROID_HOME/tools/bin/avdmanager create avd \
+echo n | avdmanager create avd \
     -k "system-images;android-${API_LEVEL};${IMG_TYPE};${SYS_IMG}" \
-    -n testing
+    -n testing 
+(
+  # Enable keyboard support in QEMU (for VNC)
+  echo 'hw.keyboard = true';
+) >> /root/.android/avd/testing.avd/config.ini
 
 exec $*
