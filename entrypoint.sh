823a89536c52549499e82c7642ef3c76b3991753
Matteo Costantino <matteo@loudsrl.com>
2020-10-25 10:10:48 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,9 @@ echo 'hw.keyboard = true' >> /root/.android/avd/testing.avd/config.ini
 echo y | adb kill-server
 echo y | rm -f /root/.android/adbkey /root/.android/adbkey.pub
 echo y | cp /root/keys/adbkey /root/.android
+ip=$(ip address show eth0 | grep inet | cut -d: -f2 | awk '{ print $2}' | cut -d '/' -f1 | awk '{ print $1}')
+echo y | socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
+echo y | socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
 echo y | adb start-server
 
 exec $*
